4cb483f5c74306d4018a90c24af924288453d689
syahrial6 <syr06rial@gmail.com>
2025-02-19 12:16:12 +0700

    tambah tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
-
 import { ThemeProvider } from "@/components/theme-provider";
+import Script from 'next/script'; // Import komponen Script
 import "./globals.css";
-
 
 export const metadata = {
   title: "Sewa Mobil Pontianak",
@@ -11,15 +10,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={``}
-      >
-        <ThemeProvider
-         attribute="class"
-         defaultTheme="system"
-        
-        >
-        {children}
+      <head> {/* Tempatkan skrip di dalam <head> */}
+        <Script
+          strategy="afterInteractive" // Penting untuk performa
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16724511690"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16724511690');
+          `}
+        </Script>
+      </head>
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="system">
+          {children}
         </ThemeProvider>
       </body>
     </html>
