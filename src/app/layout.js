09eae27d8eb94086e515c4293d7ef8cb68970e5e
syahrial6 <syr06rial@gmail.com>
2025-02-19 14:34:12 +0700

    tambah head

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import Script from 'next/script'; // Import komponen Script
 import "./globals.css";
+import Head from "next/head";
+
 
 export const metadata = {
   title: "Sewa Mobil Pontianak",
@@ -10,8 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head> {/* Tempatkan skrip di dalam <head> */}
-        <Script
+      <Head>
+      <meta name="Sewa Mobil Pontianak" content="Butuh Mobil di Pontianak? CV Artha Royal Group Adalah Penyewaan Mobil Resmi Di Pontianak Terpercaya & Terlengkap" />
+      <meta name="robots" content="index, follow" />
+      </Head>
+      <Script
           strategy="afterInteractive" // Penting untuk performa
           src="https://www.googletagmanager.com/gtag/js?id=AW-16724511690"
         />
@@ -23,7 +28,6 @@ export default function RootLayout({ children }) {
             gtag('config', 'AW-16724511690');
           `}
         </Script>
-      </head>
       <meta name="google-site-verification" content="qCRe_lfvgC1b9F50m9ICzP4d-kElThyrzFoFetwqAvU" />
       <body>
         <ThemeProvider attribute="class" defaultTheme="system">
