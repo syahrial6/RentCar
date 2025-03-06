5783d871dafa79d54975aa51fad8f244f9ceb255
syahrial6 <syr06rial@gmail.com>
2025-03-06 10:17:32 +0700

    fix3

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,10 @@
 import { ThemeProvider } from "@/components/theme-provider";
-
-import Script from "next/script";
+import Script from 'next/script';
 import "./globals.css";
-
-// Config SEO
-
 
 export const metadata = {
   title: "Sewa Mobil Pontianak",
-  description:
-    "Butuh Mobil di Pontianak? CV Artha Royal Group adalah Penyewaan Mobil Resmi di Pontianak Terpercaya & Terlengkap",
+  description: "Butuh Mobil di Pontianak? CV Artha Royal Group Adalah Penyewaan Mobil Resmi Di Pontianak Terpercaya & Terlengkap",
   robots: "index, follow",
   verification: {
     google: "qCRe_lfvgC1b9F50m9ICzP4d-kElThyrzFoFetwqAvU",
@@ -19,7 +14,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="id" suppressHydrationWarning>
-      <head />
       <body>
         <Script
           strategy="afterInteractive"
@@ -33,7 +27,6 @@ export default function RootLayout({ children }) {
             gtag('config', 'AW-16724511690');
           `}
         </Script>
-
         <ThemeProvider attribute="class" defaultTheme="system">
           {children}
         </ThemeProvider>
--- src/app/page.js
+++ src/app/page.js
@@ -1,58 +1,40 @@
-"use client";
-
+"use client"
 import About from "@/components/about";
 import Footer from "@/components/footer";
 import Hero from "@/components/hero";
 import CatalogSection from "@/components/katalog";
 import Navbar from "@/components/navbar";
+import { useTheme } from "next-themes"
 import Image from "next/image";
-import { DefaultSeo } from "next-seo";
 
 export default function Home() {
 
-  const SEO = {
-    title: "Sewa Mobil Pontianak - CV Artha Royal Group",
-    description:
-      "Butuh Mobil di Pontianak? CV Artha Royal Group adalah Penyewaan Mobil Resmi...",
-    openGraph: {
-      type: "website",
-      locale: "id_ID",
-      url: "https://rentalmobilpnk.com/",
-      siteName: "Rental Mobil Pontianak",
-      images: [
-        {
-          url: "https://rentalmobilpnk.com/og-image.jpg",
-          width: 1200,
-          height: 630,
-          alt: "Sewa Mobil Pontianak",
-        },
-      ],
-    },
-    twitter: {
-      cardType: "summary_large_image",
-    },
-  };
   return (
-    <>
-       <DefaultSeo {...SEO} />
-
-      <div>
-        <Navbar />
-        <Hero />
-        <About />
-        <CatalogSection />
-        <Footer />
-        <div className="fixed bottom-5 right-5 z-50">
-          <a
-            href="https://wa.me/6281349988354?text=Halo%20CV%20Artha%20Royal%20Group,%20Saya%20Ingin%20Menyewa%20Mobil%20Dari%20Website"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex items-center justify-center w-16 h-16 bg-white text-white rounded-full shadow-lg hover:bg-white hover:scale-110"
-          >
-            <Image src="/call.svg" width={50} height={50} alt="Whatsapp" />
-          </a>
-        </div>
+    <div>
+       <Navbar/>
+      <Hero/>
+      <About/>
+     
+      <CatalogSection/>
+      <Footer/>
+      <div className="fixed bottom-5 right-5 z-50">
+        <a
+          href="https://wa.me/6281349988354?text=Halo%20CV%20Artha%20Royal%20Group,%20Saya%20Ingin%20Menyewa%20Mobil%20Dari%20Website"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center justify-center w-16 h-16 bg-white text-white rounded-full shadow-lg hover:bg-white  hover:scale-110"
+        >
+          <Image src="/call.svg"
+          width={50}
+          height={50}
+          alt="Whatsapp"
+          />
+        </a>
       </div>
-    </>
+      {/* <p className="text font-primary font-bold text-2xl">Heloo World</p>
+      <button onClick={() => setTheme("dark")}>Dark</button>
+      <button onClick={() => setTheme("light")}>Light</button>
+      */}
+    </div>
   );
 }
