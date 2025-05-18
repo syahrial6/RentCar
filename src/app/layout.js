f86c90cd4e177f9d6f83155b7dafe54c699563e4
syahrial6 <syr06rial@gmail.com>
2025-05-18 20:22:23 +0700

    add_conversi

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,11 @@
 import { ThemeProvider } from "@/components/theme-provider";
-import Script from 'next/script';
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata = {
   title: "Sewa Mobil Pontianak",
-  description: "Butuh Mobil di Pontianak? CV Artha Royal Group Adalah Penyewaan Mobil Resmi Di Pontianak Terpercaya & Terlengkap",
+  description:
+    "Butuh Mobil di Pontianak? CV Artha Royal Group Adalah Penyewaan Mobil Resmi Di Pontianak Terpercaya & Terlengkap",
   robots: "index, follow",
   verification: {
     google: "qCRe_lfvgC1b9F50m9ICzP4d-kElThyrzFoFetwqAvU",
@@ -14,19 +15,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="id" suppressHydrationWarning>
-      <body>
+      <head>
         <Script
-          strategy="afterInteractive"
+          async
           src="https://www.googletagmanager.com/gtag/js?id=AW-16724511690"
         />
         <Script id="gtag-init" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-16724511690');
+           window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-16724511690');
           `}
         </Script>
+      </head>
+      <body>
         <ThemeProvider attribute="class" defaultTheme="system">
           {children}
         </ThemeProvider>
