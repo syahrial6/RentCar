cc1ca1df00518f02f472d59896573ed3d75b185e
syahrial6 <syr06rial@gmail.com>
2025-05-22 09:57:56 +0700

    tambah_tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,17 +16,25 @@ export default function RootLayout({ children }) {
   return (
     <html lang="id" suppressHydrationWarning>
       <head>
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-16724511690"
-        />
-        <Script id="gtag-init" strategy="afterInteractive">
-          {`
-           window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-16724511690');
-          `}
+        {/* Google Tag Manager */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NPKX95FW');`}
         </Script>
+        {/* End Google Tag Manager */}
       </head>
       <body>
+          <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NPKX95FW"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <ThemeProvider attribute="class" defaultTheme="system">
           {children}
         </ThemeProvider>
