8ebf8272c18704520b9691f3e45464e2909b0869
syahrial6 <syr06rial@gmail.com>
2025-09-17 09:32:39 +0700

    add_ads

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,15 +16,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang="id" suppressHydrationWarning>
       <head>
-        {/* Google Tag Manager */}
-        <Script id="gtm-head" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NPKX95FW');`}
-        </Script>
-        {/* End Google Tag Manager */}
+    
+<Script async src="https://www.googletagmanager.com/gtag/js?id=AW-17104611193" />
+<Script
+  id="gtag-init"
+  dangerouslySetInnerHTML={{
+    __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'AW-17104611193');
+    `,
+  }}
+/>
+  
       </head>
       <body>
           <noscript>
